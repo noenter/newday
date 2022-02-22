c1a4251e5eaccbb46fa82d9fc6830e527fffb565
noenter <noenter.serov@gmail.com>
2022-02-22 08:13:41 +0300

    21-02

--- start.cpp
+++ start.cpp
@@ -1,17 +1,19 @@
 #include <Windows.h>
 #include "start.h"
-
+#include <stdlib.h>
+#include <string>
 
 
 POINT CurPos;
 int x, y;
-
-string to_string(int n)
-{
-	char buf[40];
-	sprintf(buf, "%d", n);
-	return buf;
+void GetCurPos() {
+	GetCursorPos(&CurPos);
+	x = (int)CurPos.x;
+	y = (int)CurPos.y;
+	//char str[80];
+	SetWindowTextA(hStaticControl, ("X-" + std::to_string(x) + " Y-" + std::to_string(y)).c_str());
 }
+
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow) {
 
@@ -22,13 +24,16 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdsho
 	MSG SoftwareMainMessage = { 0 };
 
 	CreateWindowEx(WS_EX_TOPMOST, L"MainWndClass", L"First C++ window", WS_OVERLAPPEDWINDOW | WS_VISIBLE | WS_BORDER , 824, 0, 200, 300, NULL, NULL, NULL, NULL);
-
+	
 	while (GetMessage(&SoftwareMainMessage, NULL, NULL, NULL)) {
 		TranslateMessage(&SoftwareMainMessage);
 		DispatchMessage(&SoftwareMainMessage);
 	}
 	return 0;
-}
+
+	
+	}
+
 
 WNDCLASS NewWindowClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hinst, HICON Icon, LPCWSTR Name, WNDPROC Procedure) {
 		WNDCLASS NWC = { 0 };
@@ -45,12 +50,9 @@ WNDCLASS NewWindowClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hinst, HICON I
 	
 LRESULT CALLBACK SoftwareMainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
 
-	GetCursorPos(&CurPos);
-	x = (int)CurPos.x;
-	y = (int)CurPos.y;
-	SetWindowTextA(hWnd, a);
-
+	
 	switch (msg) {
+
 	case WM_COMMAND:
 		switch (wp) {
 		case OnButtonR1Clicked:
@@ -77,14 +79,29 @@ LRESULT CALLBACK SoftwareMainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp
 			Sleep(500);
 			turnleft(100);
 			break;
+		case OnButtonStartGUIClicked:
+			stgui = false;
+			break;
+		case OnButtonStopGUIClicked:
+			isTreading = false;
+			stgui = true;
+			CloseHandle(thread2);
+			ExitThread(0);
+			break;
+
 		default: break;
 		}
 		break;
 	case WM_CREATE:
 		MainWndAddWidgets(hWnd);
+		thread = CreateThread(NULL, 0, thread2, NULL, 0, NULL);
 		break;
 	case WM_DESTROY:
+		isTreading = false;
+		stgui = true;
+		CloseHandle(thread2);
 		PostQuitMessage(0);
+
 		break;
 	default: return DefWindowProc(hWnd, msg, wp, lp);
 	}
@@ -92,9 +109,9 @@ LRESULT CALLBACK SoftwareMainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp
 
 void MainWndAddWidgets(HWND hWnd) {
 
-	CreateWindowA("static", "Hello Window!", WS_VISIBLE | WS_CHILD, 5, 5, 490, 20, hWnd, NULL, NULL, NULL, NULL);
+	hStaticControl = CreateWindowA("static", "Hello Window!", WS_VISIBLE | WS_CHILD, 5, 5, 490, 20, hWnd, NULL, NULL, NULL, NULL);
 
-	CreateWindowA("edit", "dfgdfgdgdg", WS_VISIBLE | WS_CHILD, 5, 30, 490, 20, hWnd, NULL, NULL, NULL, NULL);
+	hEditControl = CreateWindowA("edit", "dfgdfgdgdg", WS_VISIBLE | WS_CHILD, 5, 30, 490, 20, hWnd, NULL, NULL, NULL, NULL);
 
 //	CreateWindowA("button", "Click me!", WS_VISIBLE | WS_CHILD | ES_CENTER, 5, 60, 120, 30, hWnd, (HMENU)OnButtonClicked, NULL, NULL, NULL);
 	CreateWindowA("button", "R-1", WS_VISIBLE | WS_CHILD | ES_CENTER, 0, 60, 32, 18, hWnd, (HMENU)OnButtonR1Clicked, NULL, NULL, NULL);
@@ -104,6 +121,9 @@ void MainWndAddWidgets(HWND hWnd) {
 	CreateWindowA("button", "L-1", WS_VISIBLE | WS_CHILD | ES_CENTER, 0, 80, 32, 18, hWnd, (HMENU)OnButtonL1Clicked, NULL, NULL, NULL);
 	CreateWindowA("button", "L-10", WS_VISIBLE | WS_CHILD | ES_CENTER, 32, 80, 40, 18, hWnd, (HMENU)OnButtonL10Clicked, NULL, NULL, NULL);
 	CreateWindowA("button", "L-100", WS_VISIBLE | WS_CHILD | ES_CENTER, 72, 80, 48, 18, hWnd, (HMENU)OnButtonL100Clicked, NULL, NULL, NULL);
+
+	CreateWindowA("button", "StartGUI", WS_VISIBLE | WS_CHILD | ES_CENTER, 10, 110, 60, 18, hWnd, (HMENU)OnButtonStartGUIClicked, NULL, NULL, NULL);
+	CreateWindowA("button", "StopGUI", WS_VISIBLE | WS_CHILD | ES_CENTER, 10, 150, 60, 18, hWnd, (HMENU)OnButtonStopGUIClicked, NULL, NULL, NULL);
 
 
 }
--- start.h
+++ start.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <iostream>
+#include <wingdi.h>
+#include <string>
 using namespace std;
 
 #define OnButtonR1Clicked		5
@@ -9,10 +11,227 @@ using namespace std;
 #define OnButtonL1Clicked		8
 #define OnButtonL10Clicked		9
 #define OnButtonL100Clicked		10
+
+#define OnButtonStartGUIClicked	11
+#define OnButtonStopGUIClicked	12
+
+int idmob = 0;
+volatile bool stgui = true;
+volatile bool isTreading = true;
+
+HWND hEditControl;
+HWND hStaticControl;
+HANDLE thread;
+
 LRESULT CALLBACK SoftwareMainProcedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp);
 WNDCLASS NewWindowClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hinst, HICON Icon, LPCWSTR Name, WNDPROC Procedure);
 
 void MainWndAddWidgets(HWND hWnd);
+void StartGUI();
+
+DWORD WINAPI thread2(LPVOID t) 
+{
+    while (isTreading)
+    {
+        while (stgui)
+        {
+            //SetWindowTextA(hStaticControl, "Whit");
+        }
+        StartGUI();
+
+    }
+
+    return 0;
+}
+
+void Setkeytab()
+{
+    keybd_event(0x09, 0x45, KEYEVENTF_EXTENDEDKEY | 0, 0);
+    Sleep(50);
+    keybd_event(0x09, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+    Sleep(100);
+    SetWindowTextA(hStaticControl, "Tab");
+    Sleep(500);
+
+}
+
+void Set1()
+{
+    // Simulate a key press
+    keybd_event(0x31,
+        0x45,
+        KEYEVENTF_EXTENDEDKEY | 0,
+        0);
+    Sleep(30);
+    // Simulate a key release
+    keybd_event(0x31,
+        0x45,
+        KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP,
+        0);
+    Sleep(300);
+
+}
+
+void SetF1()
+{
+    // Simulate a key press
+    keybd_event(0x70,
+        0x45,
+        KEYEVENTF_EXTENDEDKEY | 0,
+        0);
+    Sleep(30);
+    // Simulate a key release
+    keybd_event(0x70,
+        0x45,
+        KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP,
+        0);
+}
+
+void SetF5()
+{
+    // Simulate a key press
+    keybd_event(0x74,
+        0x45,
+        KEYEVENTF_EXTENDEDKEY | 0,
+        0);
+    Sleep(30);
+    // Simulate a key release
+    keybd_event(0x74,
+        0x45,
+        KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP,
+        0);
+}
+
+void Set5()
+{
+    // Simulate a key press
+    keybd_event(0x35,
+        0x45,
+        KEYEVENTF_EXTENDEDKEY | 0,
+        0);
+    Sleep(30);
+    // Simulate a key release
+    keybd_event(0x35,
+        0x45,
+        KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP,
+        0);
+}
+
+void Set2()
+{
+    // Simulate a key press
+    keybd_event(0x32,
+        0x45,
+        KEYEVENTF_EXTENDEDKEY | 0,
+        0);
+    Sleep(30);
+    // Simulate a key release
+    keybd_event(0x32,
+        0x45,
+        KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP,
+        0);
+    Sleep(300);
+
+}
+
+void Set3()
+{keybd_event(0x33, 0x45, KEYEVENTF_EXTENDEDKEY | 0, 0);
+ Sleep(30);
+ keybd_event(0x33, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+ Sleep(500);
+}
+
+void CheckTarget()
+{
+    Setkeytab();
+    SetWindowTextA(hStaticControl, "Key  -Tab");
+    int xt = 406;
+    int yt = 75;
+    while(isTreading){
+    Sleep(100);
+    SetWindowTextA(hStaticControl, "");
+    HDC dng = GetDC(NULL);
+    COLORREF c = GetPixel(dng, xt, yt);
+    int pr = (int)GetRValue(c);
+    int pg = (int)GetGValue(c);
+    int pb = (int)GetBValue(c);
+    if (pr == 205 && pg == 44 && pb == 43)
+        {
+        SetWindowTextA(hStaticControl, "Target-OK");
+        ReleaseDC(NULL, dng);
+        break;
+        }
+    ReleaseDC(NULL, dng);
+    Setkeytab();
+    //SetWindowTextA(hStaticControl, "Key  -Tab");
+    //Sleep(300);
+    }
+}
+
+
+void getpixelMob()
+{
+    int xm = 502;
+    int ym = 90;
+    while (xm < 522)
+    {
+    HDC dng = GetDC(NULL);
+    COLORREF c = GetPixel(dng, xm, ym);
+    int pr = (int)GetRValue(c);
+    int pg = (int)GetGValue(c);
+    int pb = (int)GetBValue(c);
+    SetWindowTextA(hStaticControl, ("X-" + to_string(xm) + "R-" + to_string(pr) + " G-" + to_string(pg) + " B-" + to_string(pb)).c_str());
+    Sleep(100);
+    if (pr == 255 && pg == 140 && pb == 0)
+    {
+        idmob = 1;
+        SetWindowTextA(hStaticControl, "Mob1-target");
+        Sleep(1000);
+        ReleaseDC(NULL, dng);
+        break;
+    }
+    if (pr == 96 && pg == 128 && pb == 42)
+    {
+        idmob = 2;
+        SetWindowTextA(hStaticControl, "Mob2-target");
+        Sleep(1000);
+        ReleaseDC(NULL, dng);
+        break;
+    }
+
+    //cout<<pr<<endl;
+    //cout<<(int)GetGValue(c)<<endl;
+    //cout<<(int)GetBValue(c)<<endl;
+    ReleaseDC(NULL, dng);
+    //std::cout << c << endl;
+    xm++;
+    }
+}
+
+void CheckKill()
+{
+    int xk = 406;
+    int yk = 75;
+    while (isTreading)
+    {
+        HDC dng = GetDC(NULL);
+        COLORREF c = GetPixel(dng, xk, yk);
+        int pr = (int)GetRValue(c);
+        int pg = (int)GetGValue(c);
+        int pb = (int)GetBValue(c);
+        //SetWindowTextA(hStaticControl, "Start - Kill");
+        //Sleep(500);
+        if (pr != 205 && pg != 44 && pb != 43)
+        {
+            break;
+        }
+        SetWindowTextA(hStaticControl, "Kill");
+        Set2();
+        Sleep(500);
+        ReleaseDC(NULL, dng);
+    }
+}
+
 
 void turnright(int step)
 {
@@ -43,4 +262,38 @@ void turnleft(int step)
     Sleep(200);
     mouse_event(MOUSEEVENTF_RIGHTUP, nx, 230, 0, 0);
     Sleep(300);
+}
+
+
+void StartGUI()
+{
+
+    Sleep(2000);
+    SetCursorPos(512, 230);
+    mouse_event(MOUSEEVENTF_RIGHTDOWN, 512, 230, 0, 0);
+    Sleep(10);
+    mouse_event(MOUSEEVENTF_RIGHTUP, 512, 230, 0, 0);
+    Sleep(100);
+    int t1 = 0;
+    while (isTreading)
+    {
+
+        CheckTarget();
+        CheckKill();
+        SetWindowTextA(hStaticControl, (to_string(t1)).c_str());
+        Sleep(2000);
+        t1++;
+        if (t1 == 10)
+        {
+            SetWindowTextA(hStaticControl, "Baff");
+            Set1();
+            Sleep(1800);
+            SetF5();
+            Sleep(300);
+            SetWindowTextA(hStaticControl, "Key - 3");
+            Set3();
+            Sleep(500);
+            t1 = 0;
+        }
+    }
 }
